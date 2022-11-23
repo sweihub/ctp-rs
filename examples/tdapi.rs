4baea6fc7d927328ebf83f9689864dafb35ce4b2
Sunding Wei <alex.wei@jyquant.com.cn>
2022-11-23 16:06:42 +0800

    ported tdapi to safe method

--- examples/tdapi.rs
+++ examples/tdapi.rs
@@ -37,7 +37,7 @@ pub struct Config {
 
 pub struct TDApi {
     api: Rust_CThostFtdcTraderApi,
-    spi: Option<*mut Rust_CThostFtdcTraderSpi>,
+    spi: Option<*mut CThostFtdcTraderSpi>,
     rx: Option<Receiver<String>>,
 
     pub(crate) config: Config,
@@ -63,7 +63,8 @@ impl TDApi {
     pub fn new(config: &Config) -> Self {
         let cs = std::ffi::CString::new(config.flowpath.as_bytes()).unwrap();
         let api = unsafe {
-            Rust_CThostFtdcTraderApi::new(CThostFtdcTraderApi_CreateFtdcTraderApi(cs.as_ptr()))
+            //Rust_CThostFtdcTraderApi::new(CThostFtdcTraderApi_CreateFtdcTraderApi(cs.as_ptr()))
+            *Rust_CThostFtdcTraderApi::CreateFtdcTraderApi(cs.as_ptr())
         };
         Self {
             api,
@@ -74,9 +75,10 @@ impl TDApi {
     }
 
     /// destory `self.spi`, which created by `TDApi`
-    fn drop_spi(spi: *mut Rust_CThostFtdcTraderSpi) {
-        let mut spi = unsafe { Box::from_raw(spi) };
-        spi.destruct();
+    fn drop_spi(spi: *mut CThostFtdcTraderSpi) {
+        //let mut spi = unsafe { Box::from_raw(spi) };
+        //spi.destruct();
+        unsafe {Rust_CThostFtdcTraderSpi::Destroy(spi);}
     }
 
     fn register<S: Rust_CThostFtdcTraderSpi_Trait>(&mut self, spi: S) {
@@ -88,42 +90,14 @@ impl TDApi {
         let spi: Box<Box<dyn Rust_CThostFtdcTraderSpi_Trait>> = Box::new(Box::new(spi));
         let ptr = Box::into_raw(spi) as *mut _ as *mut c_void;
 
-        // Here is the trick part:
-        //
-        // 1. `RegisterSpi` recv a `CThostFtdcTraderSpi*`, which can be cast from
-        // Rust_CThostFtdcTraderSpi, because `Rust_CThostFtdcTraderSpi` inherit from `CThostFtdcTraderSpi`
-        // in wrapper.hpp.
-        //
-        // 2. What `RegisterSpi` recved must be valid afterwards. We can use pin or boxed pointer to achieve
-        // that. What we can't do is code below, which use `Rust_CThostFtdcTraderSpi` as spi instead of a
-        // pointer `*mut Rust_CThostFtdcTraderSpi`:
-        //
-        // ```rust
-        // let mut spi_stub = unsafe { Rust_CThostFtdcTraderSpi::new(ptr) } ;
-        // let spi: *mut Rust_CThostFtdcTraderSpi = &mut spi_stub;
-        //
-        // unsafe { self.api.RegisterSpi(spi as _); }
-        //
-        // self.spi = Some(spi_stub);
-        // ```
-        //
-        // but we can do this:
-        //
-        // ```rust
-        // let mut spi_stub = unsafe { Rust_CThostFtdcTraderSpi::new(ptr) } ;
-        // self.spi = Some(spi_stub);
-        // let spi: *mut Rust_CThostFtdcTraderSpi = self.spi.as_mut().unwrap() as *mut _;
-        //
-        // unsafe { self.api.RegisterSpi(spi as _); }
-        // ```
-        //
-        // Because original code move spi_stub into self.spi after RegisterSpi, which cause original
-        // address invalid.
-        let spi_stub = Rust_CThostFtdcTraderSpi::new(ptr);
-        let spi: *mut Rust_CThostFtdcTraderSpi = Box::into_raw(Box::new(spi_stub));
-        self.api.RegisterSpi(spi as _);
-
-        self.spi = Some(spi);
+        // No magic here
+        // 1. send the Rust trait pointer
+        // 2. create the spi in C++
+        unsafe {
+            let stub = Rust_CThostFtdcTraderSpi::Create(ptr);
+            self.api.RegisterSpi(stub);
+            self.spi = Some(stub);
+        }        
     }
 
     pub fn req_init(&mut self) -> Result<(), String> {
@@ -135,15 +109,17 @@ impl TDApi {
         if self.config.front_addr.len() > 0 {
             debug!("cs is: {}", self.config.front_addr);
             let cs = CString::new(self.config.front_addr.as_bytes()).unwrap();
-            self.api.RegisterFront(cs.as_ptr() as *mut _);
+            unsafe {self.api.RegisterFront(cs.as_ptr() as *mut _)};
         }
 
+        unsafe {
         self.api
             .SubscribePrivateTopic(self.config.private_resume as _);
         self.api
             .SubscribePublicTopic(self.config.public_resume as _);
 
         self.api.Init();
+        }
 
         Ok(())
     }
@@ -158,7 +134,8 @@ impl Default for Resume {
 impl Drop for TDApi {
     fn drop(&mut self) {
         debug!("drop api");
-        self.api.destruct();
+        //self.api.destruct();
+        unsafe {self.api.Release();}
         if let Some(spi) = self.spi {
             debug!("drop spi");
             Self::drop_spi(spi);
